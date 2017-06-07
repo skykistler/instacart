bc725cd0eedd4731ab9d523dafe514c80d1c300a
Sky <sky@smudgr.io>
2017-06-07 13:09:42 -0400

    tars

--- src/me/skykistler/dsm/assoc/TARS.java
+++ src/me/skykistler/dsm/assoc/TARS.java
@@ -21,7 +21,47 @@ public class TarsMiner {
 
 	public void getActiveTars(baskets, t, T) {
 		That = new ArrayList<Tars>();
-		Y = L.clone();
+		Yam = L.clone();
+		
+		baskets.sortDescending()
+		
+		for (int j = 0; j < baskets.size(); j++) {
+			int i = j - 1;
+			int aprev = Tj - Ti;
+			
+			for (X in basket[i]) {
+				for (Y in basket[j]) {
+					
+					y = X -> Y;
+					if (Yam.contains(y) || a1 <= aprev <= a2) {
+						
+						if (That.contains(y)) {
+							y.Q <- Qy + 1;
+							y.L <- Ti;
+							
+							if (y.Q > q) {
+								That.remove(y); // failing
+								Yam.remove(y);
+							}
+
+							
+							if (y.L - Ti > q . (a1 - a2)) {
+								Yam.remove(y); // passing
+							}
+						} else {
+							That.push(y);
+							y.Q = 1;
+							y.L = Ti;
+						}
+						
+					}
+					
+					if (Yam.empty()) return That;
+				}
+			}
+		}
+		
+		return That;
 	}
 	
 	
@@ -40,5 +80,6 @@ public class Sequence {
 	X = new int[x];
 	Y = new int[y];
 	
-	a = Tl - Th; // time length of sequence
+	a = Tl - Th; // intratime time length of sequence
+	sig = Th - Th-1; // intertime, time since last occurrence
 }
